0be95bee60fca0f797687fb16975d1dfad808383
“Emma <emm4sh4w@gmail.com>
2022-08-20 13:22:48 -0400

    Frontend styling

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,17 +1,55 @@
 import styled from "styled-components";
+import mushrooms from "../assets/mushrooms.jpg";
+import foodPlatter from "../assets/food_platter.jpg";
+import redWinePour from "../assets/red_wine_pour.jpg";
+import reviews from "../assets/reviews.png";
 
 const Homepage = () => {
     return (
         <Wrapper>
-            Homepage
+            <Tier1>
+                <Image src={redWinePour} />
+            </Tier1>
+            <Tier2>
+                <Image src={foodPlatter} />
+            </Tier2>
+            <Tier3>
+                <Image src={mushrooms} />
+            </Tier3>
+            <Tier4>
+                <Image src={reviews} />
+            </Tier4>
         </Wrapper>
     );
 };
 
 const Wrapper = styled.div`
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     width: 100%;
+`;
+
+const Tier1 = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const Tier2 = styled.div`
+    display: flex;
+`;
+
+const Tier3 = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const Tier4 = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const Image = styled.img`
+    width: 400px;
 `;
 
 export default Homepage;
