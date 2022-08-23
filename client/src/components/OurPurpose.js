a155f9429ea65c015b7feaa968048d5dc8871e33
“Emma <emm4sh4w@gmail.com>
2022-08-22 22:43:51 -0400

    Frontend styling

--- client/src/components/OurPurpose.js
+++ client/src/components/OurPurpose.js
@@ -1,13 +1,36 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import womanInKitchen from "../assets/woman_in_kitchen.jpg";
+import { useNavigate } from "react-router-dom";
 
 const OurPurpose = () => {
+
+    const create = useNavigate();
+    const goCreate = () => {
+        create("/create-menu");
+    };
+
     return (
     <Wrapper>
         <Title>Our purpose</Title>
         <Purpose>
             <Image src={womanInKitchen} />
-            <PurposeText>Happy Hour was created to...</PurposeText>
+            <Description>
+                <PurposeText>
+                    Do you love receiving friends and family?
+                    Do you find yourself struggling to come up with a new menu every time?
+                </PurposeText>
+                <PurposeText>Don't even get us started on having to find the perfect wine to complement your meal...</PurposeText>
+                <PurposeText>As a result, Happy Hour was created to solve this dilemma once and for all!</PurposeText>
+                <PurposeText>
+                    By navigating to our <CreateMenu to="/create-menu">Create Menu</CreateMenu> page, you will be able to 
+                    automatically generate a delicious three course menu - including an appetizer, a main dish and a dessert - 
+                    complete with a suggested wine pairing. The best part - the menu can be catered 
+                    to your exact food and dietary preferences!!
+                </PurposeText>
+                <PurposeText>Let's get to cooking!</PurposeText>
+                <CreateBtn onClick={goCreate} >Create menu</CreateBtn>
+            </Description>
         </Purpose>
     </Wrapper>
     );
@@ -26,6 +49,13 @@ const Title = styled.h1`
 
 const Purpose = styled.div`
     display: flex;
+    margin-top: 20px;
+`;
+
+const Description = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 const Image = styled.img`
@@ -36,7 +66,26 @@ const Image = styled.img`
 
 const PurposeText = styled.h4`
     width: 400px;
-    margin-left: 25px;
+    margin-left: 10px;
+    padding: 15px;
+`;
+
+const CreateMenu = styled(Link)`
+    color: var(--color-dark-wine);
+    font-weight: bolder;
+
+    &:focus, &:active, &:visited, &:link, &:hover {
+        color: var(--color-dark-wine);
+        text-decoration: none;
+    }
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const CreateBtn = styled.button`
+    margin: 20px;
 `;
 
 export default OurPurpose;
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,17 +1,51 @@
 import styled from "styled-components";
+import spices from "../assets/spices.jpg";
 
 const Profile = () => {
     return (
         <Wrapper>
-            Profile
+            <ProfileInfo>
+                <Title>Profile</Title>
+                <ProfileImg src={spices} />
+                <ContactInfo>
+                    <Info>Emma Shaw</Info>
+                    <Info>emma@shaw.com</Info>
+                </ContactInfo>
+            </ProfileInfo>
         </Wrapper>
     );
 };
 
 const Wrapper = styled.div`
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     width: 100%;
+`;
+
+const Title = styled.h1`
+`;
+
+const ProfileInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 250px;
+    margin: 50px;
+`;
+
+const ContactInfo = styled.div`
+    margin-top: 25px;
+    text-align: center;
+`;
+
+const ProfileImg = styled.img`
+    width: 250px;
+    height: 250px;
+    border-radius: 50%;
+`;
+
+const Info = styled.h3`
+    padding-bottom: 5px;
 `;
 
 export default Profile;
